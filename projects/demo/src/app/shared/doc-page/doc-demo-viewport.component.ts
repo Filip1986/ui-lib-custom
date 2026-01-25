6325cae1b315f5788413f4ee38896f12b78f044c
Luca Filip <filip.luca.work@gmail.com>
2026-01-25 17:05:17 +0200

    small changes to demo

--- projects/demo/src/app/shared/doc-page/doc-demo-viewport.component.ts
+++ projects/demo/src/app/shared/doc-page/doc-demo-viewport.component.ts
@@ -1,11 +1,12 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ViewportPreviewComponent } from '../viewport-preview/viewport-preview.component';
+import { Card } from 'ui-lib-custom';
 
 @Component({
   selector: 'app-doc-demo-viewport',
   standalone: true,
-  imports: [CommonModule, ViewportPreviewComponent],
+  imports: [CommonModule, ViewportPreviewComponent, Card],
   templateUrl: './doc-demo-viewport.component.html',
   styleUrl: './doc-demo-viewport.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
