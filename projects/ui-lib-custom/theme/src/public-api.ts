9c01ad99ec909ca84505de7c4b107e173976047c
Luca Filip <filip.luca.work@gmail.com>
2026-03-07 22:28:49 +0200

    some small changes build not working

--- projects/ui-lib-custom/theme/src/public-api.ts
+++
@@ -1,5 +0,0 @@
-export * from '../../src/lib/theming/theme-config.service';
-export * from '../../src/lib/theming/theme-preset.interface';
-export * from '../../src/lib/theming/theme-scope.directive';
-export * from '../../src/lib/theming/theme-directives';
-export * from '../../src/lib/theming/with-theme-scope';
